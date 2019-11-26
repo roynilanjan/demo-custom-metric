00ce50973ab04236a07c944f17da2adbac8f5c59
Roy <Nilanjan.Roy@emc.com>
2019-11-26 15:11:19 +0530

    initial commit

---
+++ src/test/java/com/example/democustommetric/DemoCustomMetricApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democustommetric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCustomMetricApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
